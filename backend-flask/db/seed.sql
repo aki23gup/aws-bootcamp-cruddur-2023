83c74ac356edb1ea0706f8e891a646503d6b20f1
Akshay Vineet <gupta9060@gmail.com>
2023-03-22 21:59:09 +0000

    updates to reflect corrent username under crud

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,8 +2,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' , 'test@email.com','MOCK'),
-  ('Andrew Bayko', 'bayko' ,'test@email.com','MOCK');
+  ('Andrew Brown', 'andrewbrown' , 'test@email','MOCK'),
+  ('Andrew Bayko', 'bayko' ,'test@email','MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
