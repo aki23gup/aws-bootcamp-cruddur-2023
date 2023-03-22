bf6cd0c4c61480d95ecf7f98bb6160d13170c6b3
Akshay Vineet <gupta9060@gmail.com>
2023-03-22 03:46:49 +0000

    posting crud now works

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -204,7 +204,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'akshayvineet'
+  user_handle  = 'andrewbrown'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
